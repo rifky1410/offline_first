e1f4309dbc1be04367b78390d483007dd10195fb
rifky-raihan <mrifkyraihan1410@gmail.com>
2026-06-29 12:31:27 +0700

    fix named argument

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,21 +1,24 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import '../config/env_config.dart';
-import '../../features/news/data/datasources/news_remote_datasource.dart';
 import '../../features/news/data/repositories/news_repository_impl.dart';
-import '../../features/news/domain/repositories/news_repository.dart';
-import '../../features/news/presentation/bloc/news_bloc.dart';
+import '../../domain/repositories/news_repository.dart';
 
 final locator = GetIt.instance;
 
 void setupLocator() {
-  // Network
-  locator.registerLazySingleton<Dio>(() => Dio(BaseOptions(baseUrl: EnvConfig.baseUrl)));
+  // Register Dio
+  locator.registerLazySingleton<Dio>(() {
+    final dio = Dio(BaseOptions(baseUrl: EnvConfig.baseUrl));
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+    return dio;
+  });
 
-  // Data
-  locator.registerLazySingleton(() => NewsRemoteDatasource(locator()));
-  locator.registerLazySingleton<NewsRepository>(() => NewsRepositoryImpl(locator()));
-
-  // Bloc/Cubit
-  locator.registerFactory(() => NewsCubit(locator()));
+  // Register News Repository
+  // PERBAIKAN: Menambahkan 'remoteDataSource:' di bawah agar sesuai dengan konstruktor
+  locator.registerLazySingleton<NewsRepository>(
+    () => NewsRepositoryImpl(
+      remoteDataSource: locator<Dio>(),
+    ),
+  );
 }
